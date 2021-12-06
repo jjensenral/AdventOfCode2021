61889a9edf7188db8ffbdf7d79245b569af126d6
jens jensen <j.jensen.ral@outlook.com>
2021-12-06 19:06:42 +0000

    description of solution of day 2

--- 06.lisp
+++ 06.lisp
@@ -3,6 +3,18 @@
 
 ;;; Requires split-sequence
 
+;;; The stuff below is a pretty straightforward implementation of Part 1
+;;; (ie tracks every fish individually).  Instead of recoding it for Part 2
+;;; (where you want to count the number of fish in each generation), I ran
+;;; out of time (had to go to work...) and just solved it as a linear algebra
+;;; problem:
+;;;
+;;; Take the matrix
+;;; {{0,0,0,0,0,0,1,0,1},{1,0,0,0,0,0,0,0,0},{0,1,0,0,0,0,0,0,0},{0,0,1,0,0,0,0,0,0},...,{0,0,0,0,0,0,0,1,0}}
+;;; and transpose it.  Square it 8 times (to get to the 256th power).  Multiple with the (column) vector
+;;; {0 83 51 56 60 50 0 0 0} (which is the number of fish in each generation in the input).  Then sum
+;;; the components of the result to get the total.
+
 (defconstant +day6-test-input+ "3,4,3,1,2")
 
 
